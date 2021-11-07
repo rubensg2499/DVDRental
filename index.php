4c59f5b780e743acd26d2c1c932e43e459d0e92a
rubensg2499 <sangoru99@hotmail.com>
2021-11-06 20:10:00 -0600

    Bootstrap añadido.

--- index.php
+++ index.php
@@ -57,3 +57,13 @@ if($response['success']){
   echo $response['message'];
 }*/
 ?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
