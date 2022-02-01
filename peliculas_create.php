5cbb1a8e8764d77be66f7c3cbf6da066e54167d2
Rubén Sánchez González <sangoru99@hotmail.com>
2022-02-01 16:21:33 -0600

    Se agregó la funcionalidad CREAR de películas.

---
+++ peliculas_create.php
@@ -0,0 +1,56 @@
+<?php
+require_once("conexion.php");
+$conection = @get_conection();
+if ($conection['success']) {
+  $idiomas = @select_from(
+    $conection, "language",
+    $columns = array('language_id', 'name'),
+    $condition = "language_id > 0 ORDER BY name"
+  );
+
+  if(isset($_POST['submit']) && !empty($_POST['titulo']) &&
+    !empty($_POST['idioma']) && !empty($_POST['duracion_renta']) &&
+    !empty($_POST['precio_renta'] && !empty($_POST['costo_remplazo']))
+  ){
+    $pelicula = array(
+      'title' => $_POST['titulo'],
+      'description' => $_POST['descripcion'],
+      'release_year' => (int)$_POST['anyo'],
+      'language_id' => $_POST['idioma'],
+      'rental_duration' => (int)$_POST['duracion_renta'],
+      'rental_rate' => (float)$_POST['precio_renta'],
+      'replacement_cost' => (float)$_POST['costo_remplazo'],
+      'rating' => $_POST['clasificacion']
+    );
+
+    $response = @insert(
+      $conection, "film", $pelicula
+    );
+    if($response['success']){
+      header('Location:peliculas_show.php');
+    }else {
+      echo '<div class="alert alert-danger" role="alert">';
+      echo $response['message'];
+      echo '</div>';
+    }
+  }
+}
+ ?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Agregar nueva película</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container" style="max-width: 500px">
+      <div class="">
+        <h3 class="text-align-center">Agregar nueva película</h2>
+      </div>
+      <?php include("peliculas_formulario.php") ?>
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ peliculas_show.php
@@ -0,0 +1,87 @@
+
+<?php
+require_once("conexion.php");
+require_once("constantes.php");
+session_start();
+if(!isset($_SESSION['staff_id'])){
+  header('Location: index.php');
+}
+
+$conection = @get_conection();
+
+$peliculas = @select_from(
+  $conection, "film",
+  $columns = array('film_id','title', 'description','release_year','rating'),
+  $condition = "film_id > 0 ORDER BY last_update DESC"
+);
+
+//var_dump($peliculas);
+?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Menú DVDRental</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.menu.css">
+  </head>
+
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="#">DVDRental</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link btn btn-success" style="color:white" href="peliculas_show.php">Películas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="clientes_show.php">Clientes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="categorias_show.php">Categorías</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <a class="btn btn-outline-danger" href="salir.php">Salir</a>
+        </form>
+      </div>
+    </nav>
+    <!--Cuerpo del documento Lista de peliculas-->
+    <a href="peliculas_create.php" class="btn btn-outline-success">Agregar nueva película</a>
+
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Título</th>
+          <th scope="col">Descripción</th>
+          <th scope="col">Año</th>
+          <th scope="col">Clasificación</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          foreach ($peliculas['result'] as $pelicula) {
+            echo '<tr>';
+              echo '<th scope="row">'.$pelicula['title'].'</th>';
+              echo '<td style="width:490px">'.$pelicula['description'].'</td>';
+              echo '<td>'.$pelicula['release_year'].'</td>';
+              echo '<td>'.$pelicula['rating'].'</td>';
+              echo '<td>';
+                echo '<a href="#" class="btn btn-outline-primary" style="margin-right:5px">Ver</a>';
+                echo '<a href="#" class="btn btn-outline-success" style="margin-right:5px">Editar</a>';
+                echo '<a href="#" class="btn btn-outline-danger" style="margin-right:5px">Eliminar</a>';
+              echo '</td>';
+            echo '</tr>';
+          }
+         ?>
+      </tbody>
+    </table>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
